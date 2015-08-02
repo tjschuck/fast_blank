f2ee90a631369efa99a5f49116af6f038ac211bc
T.J. Schuck <tj@getharvest.com>
2015-08-02 00:08:49 -0400

    move

--- ext/fast_blank/fast_blank.c
+++ ext/fast_blank/fast_blank.c
@@ -17,7 +17,6 @@
 static int
 ruby_version_before_2_2()
 {
-  printf(ruby_version);
   if (RUBY_API_VERSION_MAJOR < 2 || (RUBY_API_VERSION_MAJOR == 2 && RUBY_API_VERSION_MINOR < 2)) {
     return 1;
   } else {
@@ -39,7 +38,7 @@ rb_str_blank_as(VALUE str)
   while (s < e) {
     int n;
     unsigned int cc = rb_enc_codepoint_len(s, e, &n, enc);
-
+  printf(ruby_version);
     switch (cc) {
       case 9:
       case 0xa:
